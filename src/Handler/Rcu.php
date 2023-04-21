d5422334387eb9da153574aef0d1ba6e15edbaa0
panzheqi <527164653@qq.com>
2023-04-21 11:15:52 +0800

    rcu handle

--- src/Handler/Rcu.php
+++ src/Handler/Rcu.php
@@ -5,15 +5,34 @@
  * Date: 4/19/23
  * Time: 5:39 PM
  */
-namespace  Guanfangge\RcuHotels\Handler;
-class  Rcu{
 
-    public function getDevice(\Guanfangge\RcuHotels\Contracts\Rcu $class,array $data){
-        $this->class = $class;
+namespace Guanfangge\RcuHotels\Handler;
+
+use Guanfangge\RcuHotels\Control\RcuGeekLink;
+use Guanfangge\RcuHotels\Control\RcuMiuLink;
+use Guanfangge\RcuHotels\Control\RcuThinke;
+
+class  Rcu
+{
+    public function __construct($type, $config = "")
+    {
+        $classs = collect([
+            "" => new RcuThinke($config),
+            "thinker" => new RcuThinke($config),
+            "miulink" => new RcuMiuLink(),
+            "geeklink" => new RcuGeekLink()
+        ]);
+        $this->class = $classs->get($type);
+    }
+
+    public function getDevice(array $data)
+    {
+
         return $this->class->getDevice(...$data);
     }
-    public function setDevice(\Guanfangge\RcuHotels\Contracts\Rcu $class,array $data){
-        $this->class = $class;
+
+    public function setDevice(array $data)
+    {
         return $this->class->setDevice(...$data);
     }
 }
